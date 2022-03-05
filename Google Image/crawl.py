920b1418b7d3930af59098ee44b6409e38dd2bcf
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-03-05 13:07:33 +0700

    small update

--- Google Image/crawl.py
+++ Google Image/crawl.py
@@ -1,6 +1,16 @@
 import os
 import time
 import csv
+from progress.bar import ChargingBar
+import pandas as pd
+from google_images_download import google_images_download 
+
+
+def crawlImages(keyword, limit=10, delay=0.1):
+    pass
+
+def saveImage():
+    pass
 
 if __name__ == '__main__':
-    pass
+    keywords_df = pd.read_csv('keyword.csv')
